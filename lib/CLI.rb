c8a653fd2d5665bd4adc9dff2c0f6d96e9a39727
github username <github email address>
2020-02-11 17:38:39 -0500

    improved cli

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -51,13 +51,14 @@ end
 
 def display_user_menu(user)
     puts "Please choose one of the following:"
-    puts "1. Favorite Teams\n2. Add a Favorite Team\n3. Edit Favorite Team\n4. Team Standings\n5. Schedule\n6. Stats\n7. Delete a Favorite Team\n8. Exit"
+    puts "1. Favorite Teams\n2. Add a Favorite Team\n3. Team Standings\n4. Schedule\n5. Stats\n6. Delete a Favorite Team\n7. Exit"
     choice = gets.chomp
     case choice
     when "1"
-        ap user.teams
+        display_teams(user)
     when "2"
-
+        display_teams
+        create_user_team_from_input
     when "3"
         
     else
@@ -68,84 +69,24 @@ def display_user_menu(user)
     ### 3.) Add next 5 to my Google Calendar
     ### 6.1.) Team Leaders
     ### 2.) Fun stats
-    display_teams
 end
 
-def display_teams
-    all_teams = Team.all.map{|team| team.name }
-    ap all_teams
-end
-
-def get_user_login
-    puts "Welcome to My NBA Calendar!"
-    puts "Please enter your usename"
-    login = gets.chomp
-end
-
-def match_user_id
-    if User.all.find{|u|u.user_name == login}
-        "Login successful"
-    end 
-    "Username does not exist"
-end
-
-def display_favorite_teams
-    userteam = UserTeam.all.find{|ut|ut.user_id == @user.id}
-    team_id = userteam.team_id 
-    team = Team.all.find{|t|t.id == team_id}
-    team
-end
-
-
-def get_user_input
-    input = gets.chomp
-end
-
-def default_menu
-    puts "Please choose one of the following:"
-    puts "1. Favorite Teams\n2. View All Teams\n3. Team Standings\n4. Schedule\n5. Stats\n6. Exit"
-end
-
-def favorite_teams_menu
-    puts "Please choose one of the following:"
-    puts "1. Add a Favorite Team\n2. Edit Favorite Teams\n3. Delete a Favorite Team\n4. Previous Menu"
-end
-
-def run
-    login = nil
-    get_user_login
-    while match_user_id
-        input = nil
-        while input != "6"
-            default_menu
-            input = get_user_input
-                if input == "1" ## SHOW CURRENT FAVORITE TEAM(S)
-                    display_favorite_teams
-                    puts "What would you like to do?"
-                        favorite_teams_menu
-                        input = nil
-                        while input != "4"
-                            input = get_user_input
-                                if input == "1" ## ADD A FAVORITE TEAM
-
-                                elsif input == "2" ## EDIT FAVORITE TEAMS
-
-                                elsif input == "3" ## DELETE FAVORITE TEAM
-
-                                end 
-                        end
-
-                elsif input == "2" ## VIEW ALL TEAMS 
-                    display_teams
-                elsif input == "3"
-
-                elsif input == "4"
-
-                elsif input == "5"
-
-                end
-        end  
+def display_teams(user=nil)
+    if user
+        user.teams.each{|team| puts "#{team.name}"}
+    else
+        Team.all.map{|team| puts "#{team.id}. #{team.name}" }
     end
+end
+
+def create_user_team_from_input
+    puts "Pick a team by ID to add to your favorites:"
+    team_id = gets.chomp
+    UserTeam.create(user_id: user.id, team_id: team_id)
+end
+
+def get_standings
+
 end
 
 def display_schedule_options
