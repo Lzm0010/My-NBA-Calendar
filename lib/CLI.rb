45188629ba05a005d486ce4b6ec7de6823a45bb5
davidmlee4 <davidmlee4@gmail.com>
2020-02-11 13:02:15 -0500

    cli updates

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,7 +1,7 @@
 
 def display_user_menu
     puts "Please choose one of the following:"
-    puts "1. Favorite Teams\n2. Add a Favorite Team\n3. Edit Favorite Team\n4. Team Standings\n5. Schedule\n6. Stats\n7. Delete a Favorite Team"
+    puts "1. Favorite Teams\n2. Add a Favorite Team\n3. Edit Favorite Team\n4. Team Standings\n5. Schedule\n6. Stats\n7. Delete a Favorite Team\n8. Exit"
     ### 5.1.) Last 5
     ### 2.) Next 5
     ### 3.) Add next 5 to my Google Calendar
@@ -9,7 +9,44 @@ def display_user_menu
     ### 2.) Fun stats
 end
 
+def print_all_teams
+    url = https://api-nba-v1.p.rapidapi.com/teams
+    response = 
 
+end
+
+def get_user_input
+    puts "Please choose one of the following:"
+    puts "1. Favorite Teams\n2. Add a Favorite Team\n3. Edit Favorite Team\n4. Team Standings\n5. Schedule\n6. Stats\n7. Delete a Favorite Team\n8. Exit"
+    input = gets.chomp
+end
+
+def run
+    input = nil
+    while input != "8"
+        input = get_user_input
+        if input = "1"
+            print_all_teams 
+
+        elsif input = "2"
+
+        elsif input = "3"
+
+        elsif input = "4"
+
+        elsif input = "5"
+
+        elsif input = "6"
+
+        elsif input = "7"
+
+
+        end
+
+
+    end
+
+end
 def display_schedule_options
 
 
---
+++ lib/Player.rb
@@ -0,0 +1,41 @@
+class Player < ActiveRecord::Base
+    belongs_to :teams
+
+    def team_id
+        
+    end
+
+    def college
+
+    end
+
+    def age
+
+    end
+
+    def number
+
+    end
+
+    def position
+
+    end
+
+    def height 
+
+    end
+
+    def weight 
+
+    end
+
+    def start_nba
+
+    end
+
+    def stats
+
+    end
+
+
+end
