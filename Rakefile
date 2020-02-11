e4b83d58396b6ab1d1756dd3b58e477fe6f88774
github username <github email address>
2020-02-11 10:25:30 -0500

    updated rakefile and comments for nba api

--- Rakefile
+++ Rakefile
@@ -1,1 +1,9 @@
 require_relative 'config/environment.rb'
+require 'sinatra/activerecord/rake'
+
+
+desc 'starts a console'
+task :console do
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
+  Pry.start
+end
--- lib/NbaApi.rb
+++ lib/NbaApi.rb
@@ -4,7 +4,7 @@ require 'openssl'
 require 'json'
 require 'awesome_print'
 
-
+###change url to get different api data for players & teams  etc
 url = URI("https://api-nba-v1.p.rapidapi.com/seasons/")
 
 http = Net::HTTP.new(url.host, url.port)
@@ -16,5 +16,6 @@ request["x-rapidapi-host"] = 'api-nba-v1.p.rapidapi.com'
 request["x-rapidapi-key"] = '9daf1f4c35mshdf9c1ef6a1e06e8p16d72bjsnfd410afab7c5'
 
 response = http.request(request)
-# hash = JSON.parse(response.body)
-ap response
+hash = JSON.parse(response.body)
+
+### access hash to get nba api data 
