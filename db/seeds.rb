4554660e9e19f7f5eb87f1125556f3de82020411
github username <github email address>
2020-02-11 15:21:13 -0500

    seeded the database

---
+++ db/seeds.rb
@@ -0,0 +1,33 @@
+require_relative '../config/environment'
+
+User.delete_all
+# UserTeam.delete_all
+Team.delete_all
+
+User.create(user_name: "Leemac33", 
+            full_name: "Lee McCormick",
+            location: "Charlotte")
+User.create(user_name: "dlee", 
+            full_name: "David Lee",
+            location: "Philadelphia")
+User.create(user_name: "Kobe824", 
+            full_name: "Kobe Bryant",
+            location: "Los Angeles")
+
+
+nba = NbaApiCommunicator.new
+teams_hash = nba.make_api_request_get_json("/teams/league/standard")
+nba_teams = teams_hash["api"]["teams"].select{|team| team["nbaFranchise"] == "1"}#.map{|nba_team| nba_team["fullName"]}
+nba_teams.each do |team|
+    Team.create(name: team["fullName"], 
+                city: team["city"], 
+                conference: team["leagues"]['standard']['confName'],
+                division: team["leagues"]["standard"]["divName"],
+                logo: team['logo'])
+end
+
+UserTeam.create(user_id: 1, team_id: 11)
+UserTeam.create(user_id: 2, team_id: 23)
+UserTeam.create(user_id: 3, team_id: 14)
+UserTeam.create(user_id: 1, team_id: 19)
+
--- lib/CLI.rb
+++ lib/CLI.rb
@@ -9,12 +9,13 @@ def display_user_menu
     ### 3.) Add next 5 to my Google Calendar
     ### 6.1.) Team Leaders
     ### 2.) Fun stats
+    display_teams
 end
 
 def display_teams
     nba = NbaApiCommunicator.new
     teams_hash = nba.make_api_request_get_json("/teams/league/standard")
-    nba_teams = teams_hash["api"]["teams"].select{|team| team["nbaFranchise"] == "1"}.map{|nba_team| nba_team["fullName"]}
+    nba_teams = teams_hash["api"]["teams"].select{|team| team["nbaFranchise"] == "1"}#.map{|nba_team| nba_team["fullName"]}
     ap nba_teams
 end
 
@@ -25,34 +26,34 @@ def get_user_input
     input = gets.chomp
 end
 
-def run
-    input = nil
-    while input != "8"
-        input = get_user_input
-        if input = "1"
-            print_all_teams 
+# def run
+#     input = nil
+#     while input != "8"
+#         input = get_user_input
+#         if input = "1"
+#             print_all_teams 
 
-        elsif input = "2"
+#         elsif input = "2"
 
-        elsif input = "3"
+#         elsif input = "3"
 
-        elsif input = "4"
+#         elsif input = "4"
 
-        elsif input = "5"
+#         elsif input = "5"
 
-        elsif input = "6"
+#         elsif input = "6"
 
-        elsif input = "7"
-
-
-        end
+#         elsif input = "7"
 
 
-    end
+#         end
 
-end
+
+#     end
+
+# end
+
 def display_schedule_options
-
 
 end
 
